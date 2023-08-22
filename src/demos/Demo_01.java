613450d6bf90963d2b5041c3cd4e4bdb3e1e90bb
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-22 14:58:33 +0300

    Demo file

---
+++ src/demos/Demo_01.java
@@ -0,0 +1,14 @@
+package demos;
+
+import java.util.Scanner;
+
+public class Demo_01 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Give your name: ");
+        String name = reader.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
