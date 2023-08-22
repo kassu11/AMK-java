8955be706c30ba947d8dc4f197047fb1c73b77fe
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-22 14:58:44 +0300

    Task_01

---
+++ src/exercise_01/chapter_02/Task_01.java
@@ -0,0 +1,14 @@
+package exercise_01.chapter_02;
+
+import java.util.Scanner;
+
+public class Task_01 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a degree in Fahrenheit: ");
+        double fahrenheit = reader.nextDouble();
+        double celsius = (fahrenheit - 32) / 1.8;
+
+        System.out.printf("Fahrenheit %.1f is %.1f in Celsius", fahrenheit, celsius);
+    }
+}
---
+++ src/exercise_01/chapter_02/Task_01.md
@@ -0,0 +1,3 @@
+# Task 1: Fahrenheit to Celsius Converter
+
+Fahrenheit to Celsius Converter Write a program that prompts the user to enter a temperature in Fahrenheit and converts it to Celsius. Display the converted temperature on the console with one decimal place.
