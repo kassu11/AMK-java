6909177dfe971bc9d6cd92fef83283aed77f0208
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-22 14:58:53 +0300

    Task_02

---
+++ src/exercise_01/chapter_02/Task_02.java
@@ -0,0 +1,19 @@
+package exercise_01.chapter_02;
+
+import java.util.Scanner;
+
+public class Task_02 {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+
+		System.out.print("Width (cm): ");
+		double width = reader.nextDouble();
+
+		System.out.print("Height (cm): ");
+		double height = reader.nextDouble();
+
+		double hypotenuse = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+
+		System.out.println(hypotenuse);
+	}
+}
---
+++ src/exercise_01/chapter_02/Task_02.md
@@ -0,0 +1,3 @@
+# Task 2; Triangles
+
+Write a program that asks the user for the lengths of the legs of a right triangle and prints the length of the hypotenuse of the triangle.
