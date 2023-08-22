658ed98ba60f1293f36b730fb681cf06ff2c2194
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-22 14:59:02 +0300

    Task_03

---
+++ src/exercise_01/chapter_02/Task_03.java
@@ -0,0 +1,18 @@
+package exercise_01.chapter_02;
+
+import java.util.Scanner;
+
+public class Task_03 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Weight (g): ");
+        double grams = reader.nextDouble();
+
+        int leiviskä = (int)(grams / 13.28 / 32 / 20);
+        int naula = (int)(grams / 13.28 / 32 % 20);
+        double luoti = (grams / 13.28) % 32;
+
+        System.out.printf("%3d grams is %d leiviskä, %d naula, and %.2f luoti.", (int)grams, leiviskä, naula, luoti);
+    }
+}
---
+++ src/exercise_01/chapter_02/Task_03.md
@@ -0,0 +1,15 @@
+# Task 3: Obsolete measures
+
+Leiviskä, naula and luoti are medieval Finnish units of measurement.
+
+One leiviskä is 20 naula.
+One naula is 32 luoti.
+One luoti is 13.28 grams.
+Write a program that asks the user for a weight in grams and converts it to medieval measurements: leiviskä, naula, and luoti. The program should work in the following way:
+
+```
+Weight (g): 30000
+30 000 grams is 3 leiviskä, 10 naula, and 13.28 luoti.
+```
+
+That is, leiviskä and naula are printed as integers, and luoti is printed with two decimal places.
