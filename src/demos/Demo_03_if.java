c7481a38fb5048f201cac8e8e2aae00dd292e0ae
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-23 09:57:29 +0300

    Create Demo_03_if.java

---
+++ src/demos/Demo_03_if.java
@@ -0,0 +1,9 @@
+package demos;
+
+public class Demo_03_if {
+    public static void main(String[] args) {
+        // if (5) { // This will not compile
+        //     System.out.println("Truthy values work");
+        // }
+    }
+}
