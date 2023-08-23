bbe70f39a4e19f23356a2d420675e0b511cc76f8
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-23 09:52:24 +0300

    Demo 2

---
+++ src/demos/Demo_02.java
@@ -0,0 +1,24 @@
+package demos;
+
+import java.util.Scanner;
+
+public class Demo_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+        int number = Integer.parseInt(scanner.nextLine());
+
+        switch (number) {
+            case 1:
+                System.out.println("The number is one.");
+                break;
+            case 2:
+                System.out.println("The number is two.");
+                break;
+            default:
+                System.out.println("The number is neither one nor two.");
+                break;
+        }
+    }
+}
