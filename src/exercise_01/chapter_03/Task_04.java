b4aaa3acdf41ad8b41ab60c58a0bec8638388b88
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-23 12:45:34 +0300

    Task_04

---
+++ src/exercise_01/chapter_03/Task_04.java
@@ -0,0 +1,38 @@
+package exercise_01.chapter_03;
+
+import java.util.Scanner;
+
+public class Task_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score, i;
+        final int ROUNDS = 10;
+
+        do {
+            score = 0;
+
+            System.out.println("Welcome to the multiplication quiz!");
+
+            for(i = 0; i < ROUNDS; i++) {
+                int a = (int)Math.ceil(Math.random() * 10);
+                int b = (int)Math.ceil(Math.random() * 10);
+
+                System.out.printf("What is %d * %d = ", a, b);
+                
+                int answer = scanner.nextInt();
+
+                if (answer == a * b) {
+                    score++;
+                } else {
+                    System.out.printf("Wrong answer! The correct answer is %d\n", a * b);
+                }
+            }
+
+            System.out.printf("You scored %d out of %d\n", score, ROUNDS);
+
+            if (score == ROUNDS) {
+                System.out.println("Congratulations! You are a master of multiplication!");
+            }
+        } while (score < ROUNDS);
+    }
+}
---
+++ src/exercise_01/chapter_03/Task_04.md
@@ -0,0 +1,3 @@
+# Task 4: Multiplication table exam
+
+In primary school, students must memorize the multiplication tables for numbers 1 to 10. Create a program to assist primary school students that presents the user with ten randomly generated multiplication problems, where the factors are integers between one and ten. After each answer, the program indicates whether it was correct or not. The user receives one point for each correctly answered question. If the user scores ten points for the entire set of problems, the program congratulates them on mastering the multiplication tables and terminates. Otherwise, the program starts a new set of problems.
