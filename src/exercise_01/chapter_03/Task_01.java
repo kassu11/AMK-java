8ff831c1404925ca73391ad6a3424de44fd298fe
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-23 09:52:36 +0300

    Task_01

---
+++ src/exercise_01/chapter_03/Task_01.java
@@ -0,0 +1,34 @@
+package exercise_01.chapter_03;
+
+import java.util.Scanner;
+
+public class Task_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the coefficients of the quadratic equation (ax^2 + bx + c = 0):");
+
+        System.out.print("Enter coefficient a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Enter coefficient b: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Enter coefficient c: ");
+        double c = scanner.nextDouble();
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("Two real roots: " + root1 + " and " + root2);
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            System.out.println("One real root: " + root);
+        } else {
+            System.out.println("No real roots");
+        }
+
+        scanner.close();
+    }
+}
---
+++ src/exercise_01/chapter_03/Task_01.md
@@ -0,0 +1,3 @@
+# Task 1: Quadratic equation solver
+
+Write a program that prompts the user to enter the coefficients of a quadratic equation (ax^2 + bx + c = 0) and calculates its roots using the quadratic formula. Display the roots on the console. If the equation has no real roots, display the message "No real roots".
