092ee0a30cb381762348b5ec408b0d054c42dd29
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-23 10:32:30 +0300

    Task_02

---
+++ src/exercise_01/chapter_03/Task_02.java
@@ -0,0 +1,15 @@
+package exercise_01.chapter_03;
+
+import java.util.Scanner;
+
+public class Task_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type a binary number: ");
+        String binary = scanner.nextLine();
+
+        int decimal = Integer.parseInt(binary, 2);
+
+        System.out.printf("The binary '%s' is %d in decimal", binary, decimal);
+    }
+}
---
+++ src/exercise_01/chapter_03/Task_02.md
@@ -0,0 +1,3 @@
+# Task 2: Binary values
+
+Write a program that prompts the user to enter a binary number (composed of 0s and 1s) and converts it to decimal. Display the decimal equivalent on the console. You may use arithmetic operations and type conversions to accomplish this task.
