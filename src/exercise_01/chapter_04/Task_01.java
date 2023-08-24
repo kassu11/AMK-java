85afc41d9a6c881fb98abdb8dda101ed9492e3ad
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-24 09:24:08 +0300

    Task_01

---
+++ src/exercise_01/chapter_04/Task_01.java
@@ -0,0 +1,29 @@
+package exercise_01.chapter_04;
+
+import java.util.Scanner;
+
+public class Task_01 {
+    public static void main(String[] args) {
+        final char[][] FIRSTNAMES = {
+            { 'K', 'a', 's', 'p', 'e', 'r' }, 
+            { 'J', 'e', 's', 'p', 'e', 'r' }
+        };
+
+        final char[][] LASTNAME = {
+            { 'M', 'ä', 'k', 'e', 'l', 'ä' },
+            { 'R', 'a', 'n', 't', 'a', 'l', 'a' }
+        };
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Type the number of names you want to generate: ");
+        int amount = scanner.nextInt();
+
+        for(int i = 0; i < amount; i++) {
+            int firstRandom = (int)Math.floor(Math.random() * FIRSTNAMES.length);
+            int lastRandom = (int)Math.floor(Math.random() * LASTNAME.length);
+
+            System.out.printf("%s %s\n", new String(FIRSTNAMES[firstRandom]), new String(LASTNAME[lastRandom]));
+        }
+    }
+}
---
+++ src/exercise_01/chapter_04/Task_01.md
@@ -0,0 +1,11 @@
+# Task 1: A name generator
+
+Write a program that generates random names by combining first and last names from hard-coded name arrays. The program should work as follows:
+
+Create two arrays, firstNames and lastNames, which contain first and last names.
+
+Ask the user how many random names the program should generate.
+
+Generate random names using the following logic: a) Generate a random index firstNameIndex from the firstNames array. b) Generate a random index lastNameIndex from the lastNames array. c) Combine the randomly chosen first name and last name to form a new name. d) Print the generated name.
+
+Repeat steps 3a-3d as many times as the user-specified number of names to generate.
