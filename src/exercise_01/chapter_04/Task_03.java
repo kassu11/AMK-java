e153dedaf95521e9ff0d9bbffdbe60a4e89c6f84
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-24 10:38:04 +0300

    Task_03

---
+++ src/exercise_01/chapter_04/Task_03.java
@@ -0,0 +1,35 @@
+package exercise_01.chapter_04;
+
+import java.util.Scanner;
+
+public class Task_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        int duplicateCount = 0;
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the integers into the array:");
+
+        main: for(int i = 0; i < size; i++) {
+            System.out.printf("Enter integer %d: ", i + 1);
+            int amount = scanner.nextInt();
+
+            for(int j = 0; j < i - duplicateCount; j++) {
+                if(array[j] == amount) {
+                    duplicateCount++;
+                    continue main;
+                }
+            }
+            
+            array[i - duplicateCount] = amount;
+        }
+
+        for(int i = 0; i < size - duplicateCount; i++) {
+            System.out.printf("%d ", array[i]);
+        }
+    }
+}
---
+++ src/exercise_01/chapter_04/Task_03.md
@@ -0,0 +1,29 @@
+# Task 3: Removing duplicates
+
+Write a program that takes an array of integers as input from the user and removes all the duplicate numbers from it. The program should work as follows:
+
+Ask the user for the size of the array.
+Prompt the user to enter the integers into the array.
+Remove all the duplicate numbers from the array using the following logic:
+Create a new array that stores only one occurrence of each number.
+Keep only the first occurrence of each number and discard any subsequent occurrences.
+Print the resulting array without the duplicate numbers.
+In this assignment, you can make the new array as big as the original array, even though it may not be completely filled.
+
+## Example:
+
+```
+Enter the size of the array: 8
+Enter the integers into the array:
+Enter integer 1: 1
+Enter integer 2: 2
+Enter integer 3: 3
+Enter integer 4: 2
+Enter integer 5: 1
+Enter integer 6: 3
+Enter integer 7: 4
+Enter integer 8: 5
+
+The array without duplicates:
+1 2 3 4 5
+```
