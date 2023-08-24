5076d1742186e83e5d3120be25f91e15dba7ca7a
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-24 10:08:57 +0300

    Task_02

---
+++ src/exercise_01/chapter_04/Task_02.java
@@ -0,0 +1,45 @@
+package exercise_01.chapter_04;
+
+import java.util.Scanner;
+
+public class Task_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the integers into the array:");
+
+        for(int i = 0; i < size; i++) {
+            System.out.printf("Enter integer %d: ", i + 1);
+            int amount = scanner.nextInt();
+            array[i] = amount;
+        }
+
+        int maxValue = 0;
+        int[] indexes = {0, 0};
+
+        for(int i = 0; i < size; i++) {
+            int sum = 0;
+            for(int j = i; j < size; j++) {
+                if (i == 0 && j == 0) {
+                    maxValue = array[0];
+                }
+
+                sum += array[j];
+
+                if(sum > maxValue) {
+                    maxValue = sum;
+                    indexes[0] = i;
+                    indexes[1] = j;
+                }
+            }
+        }
+
+        System.out.printf("\nMaximum sum: %d\n", maxValue);
+        System.out.printf("Integers: %d-%d\n", indexes[0] + 1, indexes[1] + 1);
+    }
+}
---
+++ src/exercise_01/chapter_04/Task_02.md
@@ -0,0 +1,29 @@
+# Task 2: Finding the maximum subarray sum
+
+Write a program that takes an array of integers as input from the user and finds the subarray with the maximum sum. The program should work as follows:
+
+Ask the user for the number of integers in the array.
+Prompt the user to enter the integers into the array.
+Find the subarray with the maximum sum using the following logic:
+Iterate through all possible subarrays in the array.
+Calculate the sum of each subarray.
+Keep track of the maximum sum found and the corresponding subarray indices.
+Print the maximum sum and the corresponding indices. (Use the indices shown to the user, i.e., starting from 1.)
+
+## Example:
+
+```
+Enter the size of the array: 8
+Enter the integers into the array:
+Enter integer 1: -2
+Enter integer 2: 1
+Enter integer 3: -3
+Enter integer 4: 4
+Enter integer 5: -1
+Enter integer 6: 2
+Enter integer 7: 1
+Enter integer 8: -5
+
+Maximum sum: 6
+Integers: 4-7
+```
