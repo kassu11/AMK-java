1e0dd44c1af5be22e1bb221b794e13c5c3e6fdb0
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-29 10:45:02 +0300

    Update Run_simulation.java

--- src/simulation/exercise_01/task_06/Run_simulation.java
+++ src/simulation/exercise_01/task_06/Run_simulation.java
@@ -6,13 +6,10 @@ import java.util.Scanner;
 public class Run_simulation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char answer;
-        LinkedList<Customer> list = new LinkedList<>();
         ServicePoint servicePoint = new ServicePoint();
 
         System.out.print("How many customers do you want to add: ");
         int amount = scanner.nextInt();
-        int sum = 0;
 
         for(int i = 0; i < amount; i++) {
             servicePoint.addToQueue();
