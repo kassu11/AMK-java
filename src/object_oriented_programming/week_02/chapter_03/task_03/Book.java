75c42752b40ae9b66db5c69877a5ae259b7275a1
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-31 11:51:59 +0300

    task 3

---
+++ src/object_oriented_programming/week_02/chapter_03/task_03/Book.java
@@ -0,0 +1,25 @@
+package object_oriented_programming.week_02.chapter_03.task_03;
+
+public class Book {
+    private String author;
+    private String title;
+    private String publicationYear;
+
+    public Book(String author, String title, String publicationYear) {
+        this.author = author;
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getPublicationYear() {
+        return this.publicationYear;
+    }
+}
---
+++ src/object_oriented_programming/week_02/chapter_03/task_03/readme.md
@@ -0,0 +1,5 @@
+# Task 3: Book Availability Check
+
+Add a method to the `Library` class to check the availability of a specific book by its title:
+
+- `isBookAvailable(String title)`: This method should return a boolean indicating whether the book with the specified title is available in the library.
