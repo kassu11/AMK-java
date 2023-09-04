bd4a5592d613b285adbfe2850ad6947d1f9bf706
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-09-04 15:18:27 +0300

    Update Demo_08_eduni.java

--- src/demos/Demo_08_eduni.java
+++ src/demos/Demo_08_eduni.java
@@ -1,4 +1,14 @@
 package demos;
 
+import eduni.distributions.*;
+
 public class Demo_08_eduni {
+    public static void main(String[] args) {
+        Normal test = new Normal(10, 100);
+        for(int i = 0; i < 10; i++) {
+            double t = test.sample();
+            System.out.println(t);
+        }
+
+    }
 }
