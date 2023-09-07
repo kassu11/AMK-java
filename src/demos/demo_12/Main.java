6925f065d42d83faab9d53e4d0fe06d00d330c33
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-09-07 10:42:33 +0300

    Read file

---
+++ src/demos/demo_12/Main.java
@@ -0,0 +1,30 @@
+package demos.demo_12;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        FileReader fr = null;
+        try {
+            fr = new FileReader("C:\\Users\\kaspe\\IdeaProjects\\AMK-java\\src\\demos\\demo_12\\ruoka.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("Tiedostoa ei löytynyt");
+            System.exit(1);
+        }
+        BufferedReader br = new BufferedReader(fr);
+
+        String rivi = null;
+        do {
+            try {
+                rivi = br.readLine();
+            } catch (IOException e) {
+                System.out.println("Tiedostoa ei voi lukea");
+                System.exit(2);
+            }
+            if (rivi != null) System.out.println(rivi);
+        } while(rivi != null);
+    }
+}
