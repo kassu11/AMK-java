f3f9764963d0f9afff3bd4431cc05ae231bc2bc7
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-09-07 10:59:55 +0300

    File read v2

---
+++ src/demos/demo_12/Main_v2.java
@@ -0,0 +1,22 @@
+package demos.demo_12;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main_v2 {
+    public static void main(String[] args) {
+        try (FileReader fr = new FileReader("C:\\Users\\kaspe\\IdeaProjects\\AMK-java\\src\\demos\\demo_12\\ruoka.txt"); BufferedReader br = new BufferedReader(fr)) {
+            String rivi = null;
+            do {
+                rivi = br.readLine();
+                if (rivi != null) System.out.println(rivi);
+            } while(rivi != null);
+
+        } catch (IOException e) {
+            System.out.println("Tiedoston käsittely epäonnistui");
+            System.exit(1);
+        }
+    }
+}
